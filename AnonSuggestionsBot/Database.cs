3c59cd251a26f06ec26f6ade82a1b1f74c0c5ba3
J <puffleboy101@hotmaIl.co.uk>
2023-10-22 21:12:58 +0100

    Add project files.

---
+++ AnonSuggestionsBot/Database.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonSuggestionsBot {
+    public class Database {
+
+        private NpgsqlDataSource dataSource;
+        public enum serverInitializationReturn{
+            SUCCESS,
+            ALREADY_INITIALIZED,
+            ERROR
+        }
+
+        public async void login(string IP, string password) {
+            var connectionString = string.Format("Host={0};Username=postgres;Password={1};Database=postgres", IP, password);
+            dataSource = NpgsqlDataSource.Create(connectionString);
+            selectSchema();
+        }
+
+        public async void selectSchema() {
+            await using var command = dataSource.CreateCommand("SET schema 'AnonSuggestionsBot'");
+            await using var reader = await command.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync()) {
+                Console.WriteLine("DB RESPONSE: " + reader.GetString(0));
+            }
+        }
+
+        public async void createServerEntry(ulong discord_id, string inputChannel, string outputChannel) {
+            await using var getCurrentDiscordIdInitialized = dataSource.CreateCommand(string.Format("select count(server_id) from \"AnonSuggestionsBot\".servers where discord_id like '{0}';", discord_id));
+            await using var getCurrentDiscordIdInitializedReader = await getCurrentDiscordIdInitialized.ExecuteReaderAsync();
+
+            while (getCurrentDiscordIdInitializedReader.Read()) {
+                if (getCurrentDiscordIdInitializedReader.GetValue(0).ToString() != "0") {
+                    await using var deleteOldDiscordEntry = dataSource.CreateCommand(string.Format("delete from \"AnonSuggestionsBot\".servers where discord_id like '{0}';", discord_id));
+                    await deleteOldDiscordEntry.ExecuteReaderAsync();
+                }
+
+                await using var createCurrentDiscordEntry = dataSource.CreateCommand(string.Format("insert into \"AnonSuggestionsBot\".servers (discord_id, discord_submit_channel, discord_post_channel) values ({0}, '{1}', '{2}');", discord_id, inputChannel, outputChannel));
+                await createCurrentDiscordEntry.ExecuteReaderAsync();
+            }
+        }
+
+        public async Task<ulong> lookupServerOutputChannel(ulong discord_id) {
+            await using var getOutputChannel = dataSource.CreateCommand(string.Format("select discord_post_channel from \"AnonSuggestionsBot\".servers where discord_id like '{0}';", discord_id));
+            await using var getOutputChannelReader = await getOutputChannel.ExecuteReaderAsync();
+
+            while (getOutputChannelReader.Read()) {
+                return ulong.Parse(getOutputChannelReader.GetString(0));
+            }
+
+            return 0;
+        }
+
+        public async Task<string> getBotToken() {
+            await using var getBotToken = dataSource.CreateCommand("select bot_token from \"AnonSuggestionsBot\".config;");
+            await using var getBotTokenReader = await getBotToken.ExecuteReaderAsync();
+
+            while (getBotTokenReader.Read()) {
+                return getBotTokenReader.GetString(0);
+            }
+
+            return "";
+        }
+
+        public async void logSuggestion(ulong serverID, string userID_anonymised, string suggestionUID, string suggestion) {
+
+        }
+    }
+}
---
+++ AnonSuggestionsBot/Program.cs
@@ -0,0 +1,144 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.Net;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace AnonSuggestionsBot
+{
+    internal class Program {
+        private static DiscordSocketClient _client;
+        private static IServiceProvider _services;
+        private static Database _db = new Database();
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync() {
+            Console.Write("Enter postgresql IP: ");
+            string? IPAddy = Console.ReadLine();
+            Console.Write("\nEnter postgresql password: ");
+            string? password = Console.ReadLine();
+            Console.Clear();
+
+            _db.login(IPAddy, password); //login to the local DB        
+
+            string token = await _db.getBotToken();
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig {
+                LogLevel = LogSeverity.Info,
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            });
+
+            var commandService = new CommandService(new CommandServiceConfig {
+                CaseSensitiveCommands = false
+            });
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(commandService)
+                .AddSingleton<InteractionService>()
+                .BuildServiceProvider();
+
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.ButtonExecuted += buttonHandler;
+            _client.ModalSubmitted += modalResponseHandler;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task LogAsync(LogMessage log) {
+            Console.WriteLine(log);
+        }
+
+        private async Task ReadyAsync() {
+            _client.SlashCommandExecuted += SlashCommandHandler;
+
+            //initialize the global /initialize command
+            var initializeCmd = new SlashCommandBuilder();
+            initializeCmd.WithName("initialize");
+            initializeCmd.WithDescription("initializes AnonSuggestionsBot for this server");
+            initializeCmd.AddOption("input", ApplicationCommandOptionType.Channel, "the channel to post the create suggestions message in");
+            initializeCmd.AddOption("output", ApplicationCommandOptionType.Channel, "the channel to send anonymous suggestions to");
+            await _client.CreateGlobalApplicationCommandAsync(initializeCmd.Build());
+        }
+
+        private async Task SlashCommandHandler(SocketSlashCommand command) {
+            if(command.CommandName == "initialize") {
+                await guildSetup(command);
+            }
+        }
+
+        private async Task guildSetup(SocketSlashCommand command) {
+            if(command.GuildId == null) { return; }
+            SocketGuild guild = _client.GetGuild((ulong)command.GuildId);
+
+            if(command.Data.Options.Count() < 2) {
+                await command.RespondAsync("You must specify both an input and output channel");
+                return;
+            }
+
+            SocketGuildChannel? inputChannel = guild.Channels.FirstOrDefault(c => c.Name == command.Data.Options.ToArray()[0].Value.ToString());
+            SocketGuildChannel? outputChannel = guild.Channels.FirstOrDefault(c => c.Name == command.Data.Options.ToArray()[1].Value.ToString());
+
+            if (inputChannel == null || outputChannel == null) {
+                await command.RespondAsync("You must specify both an input and output channel");
+                return;
+            }
+
+            _db.createServerEntry((ulong)command.GuildId, inputChannel.Id.ToString(), outputChannel.Id.ToString());
+
+            var suggestionBan = new SlashCommandBuilder();
+            suggestionBan.WithName("suggestion-ban");
+            suggestionBan.WithDescription("Bans the creator of suggestion");
+            suggestionBan.AddOption("suggestion", ApplicationCommandOptionType.String, "the ID of the suggestion to ban the creator of");
+            await guild.CreateApplicationCommandAsync(suggestionBan.Build());
+
+            var btnBuilder = new ComponentBuilder().WithButton("Suggest", "btn-send-suggestion");
+            await guild.GetTextChannel(inputChannel.Id).SendMessageAsync("Click here to submit a suggestion:", components: btnBuilder.Build());
+
+            await command.RespondAsync("Initialization complete");
+        }
+
+        public async Task buttonHandler(SocketMessageComponent component) {
+        if(component.Data.CustomId == "btn-send-suggestion") {
+                var mb = new ModalBuilder();
+                mb.WithTitle("AnonSuggestionBot");
+                mb.WithCustomId("suggestion-input");
+                mb.AddTextInput("Title:", "suggestion-input-title", TextInputStyle.Short);
+                mb.AddTextInput("Suggestion:", "suggestion-input-body", TextInputStyle.Paragraph);
+
+                await component.RespondWithModalAsync(mb.Build());
+            }
+        }
+
+        public async Task modalResponseHandler(SocketModal modal) {
+            ulong? guildId = modal.GuildId;
+            if(guildId == null) { return; }
+            SocketGuild guild = _client.GetGuild((ulong)guildId);
+            string title = modal.Data.Components.First(x => x.CustomId == "suggestion-input-title").Value;
+            string body = modal.Data.Components.First(x => x.CustomId == "suggestion-input-body").Value;
+
+            ulong outputChannelId = await _db.lookupServerOutputChannel((ulong)guildId);
+
+            SocketTextChannel? outputChannel = guild.GetTextChannel(outputChannelId);
+
+            if(outputChannel == null) { return; }
+
+            var embed = new EmbedBuilder();
+            embed.AddField(title, body);
+            embed.WithFooter("Suggestion ID: placeholder");
+
+            await outputChannel.SendMessageAsync(embed: embed.Build());
+
+            await modal.RespondAsync();
+        }
+    }
+}
